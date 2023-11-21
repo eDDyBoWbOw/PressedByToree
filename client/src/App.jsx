55d0bf53d551ef80e27608b35a2730f37d64a0ec
Alex Merida Castillo <mcastilloalex@gmail.com>
2023-11-20 19:44:47 -0700

    updated home component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,15 @@ import './App.css'
 import Home from '../pages/Home'
 import Login from '../pages/Login'
 
+
 function App() {
 
 
   return (
-    <>
+    <div>
       <Home></Home>
       <Login></Login>
-    </>
+    </div>
   )
 }
 
