4b690c975245aa1ff01fcbf0bd3ce04b50dbc85c
Alex Merida Castillo <mcastilloalex@gmail.com>
2023-11-27 17:49:19 -0700

    Changed components/pages to Product// updated path main.jsx to Product

---
+++ client/src/pages/Product/index.jsx
@@ -0,0 +1,16 @@
+import './style.css'
+import NavbarComponent from '../../components/Navbar'
+
+
+function Products() {
+  return (
+    <>
+      <NavbarComponent />
+      <p>This is the products page</p>
+
+
+    </>
+  )
+}
+
+export default Products
